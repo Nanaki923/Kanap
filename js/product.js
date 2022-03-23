1733a24b0917102177c6ef6630d4f9d428f31e6b
Nanaki923 <pezzanodavid@hotmail.fr>
2022-03-23 17:45:36 +0100

    creation fiches articles

--- js/product.js
+++ js/product.js
@@ -1,15 +1,25 @@
-fetch("http://localhost:3000/api/products")
+const url = new URL(window.location.href);
+const id = url.searchParams.get("id")
+console.log(id)
+
+fetch(`http://localhost:3000/api/products/${id}`)
     .then((response) => response.json())
     .then((json) => {
-        return addProducts(json)
+        console.log(json)
+        return addContent(json)
     })
 
-function addProducts(json) {
-    let products = ""
-    json.map(item => {
-        products += `<title>nom du produit</title>>
-        `
+function addContent(json) {
+    document.title = json.name
+    document.querySelector("#title").innerHTML = json.name
+    document.querySelector("#price").innerHTML = json.price
+    document.querySelector("#description").innerHTML = json.description
+    let colors = ""
+    json.colors.forEach(color => {
+        console.log(color)
+        colors += `<option value="${color}">${color}</option>`
+    });
+    console.log(colors)
+    document.querySelector("#colors").innerHTML += colors
 
-    })
-    document.querySelector("#items").innerHTML = products
 }
